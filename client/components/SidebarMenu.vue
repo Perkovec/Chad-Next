3d45145dc79ab3001491175e7e9ee3439dbfcecc
Perkovec <perkovec24@gmail.com>
2018-11-29 22:02:49 +0300

    Update vue-router, change sidebar

--- client/components/SidebarMenu.vue
+++ client/components/SidebarMenu.vue
@@ -1,30 +1,44 @@
 <template>
   <AtMenu
     theme="dark"
-    mode="vertical"
+    mode="inline"
     active-name="0"
   >
-    <AtMenuItem name="0">
-      <i class="icon icon-edit" />
-      Write post
-    </AtMenuItem>
-    <AtMenuItem name="1">
-      <i class="icon icon-message-square" />
-      Posts
-    </AtMenuItem>
-    <AtMenuItem name="2">
-      <i class="icon icon-trash" />
-      Drafts
-    </AtMenuItem>
-    <AtMenuItem name="3">
-      <i class="icon icon-settings" />
-      Preferences
-    </AtMenuItem>
-    <AtMenuItem name="4">
-      <i class="icon icon-info" />
-      About Chad
-      <sup>Next</sup>
-    </AtMenuItem>
+    <AtSubmenu>
+      <template slot="title">
+        Menu
+      </template>
+      <AtMenuItem name="0">
+        <i class="icon icon-edit" />
+        Write post
+      </AtMenuItem>
+      <AtMenuItem name="1">
+        <i class="icon icon-message-square" />
+        Posts
+      </AtMenuItem>
+      <AtMenuItem name="2">
+        <i class="icon icon-trash" />
+        Drafts
+      </AtMenuItem>
+      <AtMenuItem name="3">
+        <i class="icon icon-settings" />
+        Preferences
+      </AtMenuItem>
+      <AtMenuItem name="4">
+        <i class="icon icon-info" />
+        About Chad
+        <sup>Next</sup>
+      </AtMenuItem>
+    </AtSubmenu>
+    <AtSubmenu>
+      <template slot="title">
+        Channels
+      </template>
+      <AtMenuItem name="5">
+        <i class="icon icon-plus" />
+        Add channel
+      </AtMenuItem>
+    </AtSubmenu>
   </AtMenu>
 </template>
 
